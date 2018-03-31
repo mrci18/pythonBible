752b48814ecb8a7fc5491fbb1315416b18159a1f
mrci18 <charles.ibo.247@my.csun.edu>
2018-03-30 21:25:41 -0700

    List comprehension example

---
+++ listComprehension.py
@@ -0,0 +1,5 @@
+even_numbers = [x for x in range(1, 101) if x % 2 == 0]
+
+words = ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"]
+answer = [[w.upper(), w.lower(), len(w)]for w in words]
+print(answer)
