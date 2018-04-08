44bff4aa6e8ec61b9d24070dd7cf9d1a6bad48e3
mrci18 <charles.ibo.247@my.csun.edu>
2018-04-08 02:24:02 -0700

    Practice with constructor

---
+++ coin.py
@@ -0,0 +1,41 @@
+import random
+
+class Pound:
+
+    def __init__(self, rare=False):
+
+        self.rare = rare
+
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+
+        self.color = "gold"
+        self.num_edges = 1
+        self.diameter = 22.5 #mm
+        self.thickness = 3.15 #mm
+        self.heads = True
+
+    def rust(self):
+        self.color = "greenish"
+
+    def clean(self):
+        self.color = "gold"
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+
+    def __del__(self):
+        print("Coin Spent!")
+
+coin1 = Pound()
+coin1.value = 1.25
+
+coin2 = Pound()
+
+print(coin1.value)
+
+print(type(coin1))
