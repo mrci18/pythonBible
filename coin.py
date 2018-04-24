bfb03cbfb91beac9cd2724c7979dcbfce4dade44
mrci18 <charles.ibo.247@my.csun.edu>
2018-04-24 01:09:18 -0700

    Practice with polymorphism

--- coin.py
+++ coin.py
@@ -34,7 +34,115 @@ class Coin:
         choice = random.choice(heads_options)
         self.heads = choice
 
-class Pound(Coin):
+    def __str__(self):
+        if self.original_value > 1.00:
+            return "${} Coin".format(int(self.original_value))
+        else:
+            return "{}p Coin".format(int(orignal_value * 100))
+
+class One_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.01,
+            "clean_color": "bronze",
+            "rusty_color": "brownish",
+            "num_edges": 1,
+            "diameter": 20.3,
+            "thickness": 1.52,
+            "mass": 3.56,
+        }
+        super().__init__(**data)
+
+class Two_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.02,
+            "clean_color": "bronze",
+            "rusty_color": "brownish",
+            "num_edges": 1,
+            "diameter": 25.9,
+            "thickness": 1.85,
+            "mass": 7.12,
+        }
+        super().__init__(**data)
+
+class Five_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.05,
+            "clean_color": "silver",
+            "rusty_color": None,
+            "num_edges": 1,
+            "diameter": 18.0,
+            "thickness": 1.77,
+            "mass": 3.25,
+        }
+        super().__init__(**data)
+
+        def rust(self):
+            self.color = self.clean_color
+
+        def clean(self):
+            self.color = self.clean_color
+
+class Ten_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.10,
+            "clean_color": "silver",
+            "rusty_color": None,
+            "num_edges": 1,
+            "diameter": 24.5,
+            "thickness": 1.85,
+            "mass": 6.50,
+        }
+        super().__init__(**data)
+
+        def rust(self):
+            self.color = self.clean_color
+
+        def clean(self):
+            self.color = self.clean_color
+
+class Twenty_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.20,
+            "clean_color": "silver",
+            "rusty_color": None,
+            "num_edges": 7,
+            "diameter": 21.4,
+            "thickness": 1.7,
+            "mass": 5.00,
+        }
+        super().__init__(**data)
+
+        def rust(self):
+            self.color = self.clean_color
+
+        def clean(self):
+            self.color = self.clean_color
+
+class Fifty_Pence(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 0.50,
+            "clean_color": "silver",
+            "rusty_color": None,
+            "num_edges": 7,
+            "diameter": 27.3,
+            "thickness": 1.78,
+            "mass": 8.00,
+        }
+        super().__init__(**data)
+
+        def rust(self):
+            self.color = self.clean_color
+
+        def clean(self):
+            self.color = self.clean_color
+
+class One_Pound(Coin):
     def __init__(self):
         data = {
             "original_value": 1.00,
@@ -47,6 +155,31 @@ class Pound(Coin):
         }
         super().__init__(**data)
 
-one_pound_coin = Pound()
+class Two_Pound(Coin):
+    def __init__(self):
+        data = {
+            "original_value": 2.00,
+            "clean_color": "gold & greenish",
+            "rusty_color": "greenish",
+            "num_edges": 1,
+            "diameter": 28.4,
+            "thickness": 2.50,
+            "mass": 12.00,
+        }
+        super().__init__(**data)
 
-one_pound_coin.color
+        def rust(self):
+            self.color = self.clean_color
+
+        def clean(self):
+            self.color = self.clean_color
+
+coins = [One_Pence, Two_Pence, Five_Pence, Ten_Pence, Twenty_Pence,
+        Fifty_Pence, One_Pound, Two_Pound]
+
+for coin in coins:
+    arguements = [coin, coin.color, coin.value, coin.diameter, 
+    coin.thickness, coin.num_edges, coin.mass]
+
+    string = "{} - Color:{}, value:{}, diamter:{}, thickness:{}, number of edges:{}, mass:{}".format(*arguements)
+    print(string)
