0b2fe1e1f6574e2321e495990f1df988b229f159
Charles Ibo <charles.ibo.247@my.csun.edu>
2018-10-30 16:35:58 -0700

    Raising error note

---
+++ raiseException.py
@@ -0,0 +1,13 @@
+def add_2(num):
+    if len(num) > 2:
+        raise Exception("Numbers being multiplied must be less than 2")
+    if len(num) < 2:
+        raise Exception("Numbers being multiplied must be more than 1")
+    else:
+        return sum(num)
+if __name__ == "__main__":
+    for x in [1, 2], [3,4,5], [34]:
+        try:
+            print(add_2(x))
+        except Exception as err:
+            print("An exception happened " + str(err))
